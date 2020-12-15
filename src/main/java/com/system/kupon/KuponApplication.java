f85d545929b792d0a86244016b737a9079d10c02
Serhii Kalinichenko <kallinnas@gmail.com>
2020-12-15 14:38:59 +0200

    Initial project commit

---
+++ src/main/java/com/system/kupon/KuponApplication.java
@@ -0,0 +1,14 @@
+package com.system.kupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuponApplication.class, args);
+		System.out.println("App works just perfect!");
+	}
+
+}
---
+++ src/test/java/com/system/kupon/KuponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.kupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
